a9f7a2bfd93c020f02fc5c6ce0e7fba0719d71e6
NeptuneRjo <rjo9000@outlook.com>
2022-12-01 20:36:07 -0500

    html templates

--- djangonautic/views.py
+++ djangonautic/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home_page(request):
-    return HttpResponse('home')
+    # return HttpResponse('home')
+    return render(request, 'homepage.html')
 
 
 def about(request):
-    return HttpResponse('about')
+    # return HttpResponse('about')
+    return render(request, 'about.html')
