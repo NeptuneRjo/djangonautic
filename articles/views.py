09271e1de1e44c93e15eabf36b4b45e9b53ab266
NeptuneRjo <rjo9000@outlook.com>
2022-12-05 15:09:43 -0500

    url parameters

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Article
 
 # Create your views here.
@@ -8,3 +9,8 @@ def article_list(request):
     articles = Article.objects.all().order_by('date')
 
     return render(request, 'articles/article_list.html', {'articles': articles})
+
+
+def article_detail(request, slug):
+
+    return HttpResponse(slug)
